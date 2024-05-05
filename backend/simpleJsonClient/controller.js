856372afd73d764dedb20e2ae9ecefbc3339cfd5
tomi <tomi2002@gmx.at>
2024-05-05 13:34:12 +0200

    chat in console

--- backend/simpleJsonClient/controller.js
+++ backend/simpleJsonClient/controller.js
@@ -1,35 +1,16 @@
 $(document).ready(function () {
-    $("#searchResult").hide();
-    $("#btn_Search").click(function (e) {
-        var searchterm = $("#seachfield").val();
-        if (searchterm) {
-            loaddata(searchterm);
-        } else {
-            alert("Please enter a search term");
-        }
-    });
+    submitComment();
 });
 
-function loaddata(searchterm) {
-    $.ajax({
-        type: "GET",
-        url: "../serviceHandler.php",
-        cache: false,
-        data: {method: "queryPersonByName", param: searchterm},
-        dataType: "json",
-        success: function (response) {
-            $("#noOfentries").val(response.length);
-            $("#searchResult").show(1000).delay(1000).hide(1000);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.error(textStatus, errorThrown);
-        }
-    });
-}
-
-fetch('test_connection.php')
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+function checkForEnter(event) {
+    if (event.keyCode === 13) { 
+      event.preventDefault(); 
+      submitComment(); 
+    }
+  }
+  
+  function submitComment() {
+    var text = document.getElementById("message").value;
+    console.log("Comment: " + text); 
+    document.getElementById("message").value = ""; 
+  }
