bbe25b82cd00e3410ebaaa8f09b677c056c7dcbf
minaaa :) <amina.emurlai@gmail.com>
2024-05-05 13:24:17 +0200

    changed layout

--- frontend/index.php
+++ frontend/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css"></head>
+    <link rel="stylesheet" href="css/style.css"></head>
 <body>
     <?php include './includes/navbar.php'; ?>
     <script src="controller.js"></script>
